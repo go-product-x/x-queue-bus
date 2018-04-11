edeec4617a0732dba31980ad81ed255ee0a3101a
Michael Bouwer <michaelbouwer@Michaels-MBP.lan>
2018-04-11 17:30:06 +0200

    added comments

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-require('dotenv').config()
+//Require modules
+//require('dotenv').config() //Uncomment for use in local environment
 const express = require('express')
 const bodyParser = require('body-parser')
 
+//Instance of express
 let app = express()
 
+//Middlewares
 app.use(bodyParser.json())
 
+//Middlewares for various routes
 app.use(require('./handlers/receive-initial-transaction.js'))
 
+//Start the server to listen for requests
 app.listen(process.env.PORT || 3000, () => {
     console.log("listening");
     
